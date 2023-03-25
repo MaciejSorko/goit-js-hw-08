cc3c2e2ae1ede8e278611aa2ace29ff198a4288c
ElchaX <maciej.sorko@gmail.com>
2023-03-25 17:41:13 +0100

    task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems
+  .map(
+    galleryItem => `<a class="gallery__item" href="${galleryItem.original}">
+    <img class="gallery__image" src="${galleryItem.preview}"
+     alt="${galleryItem.description}" />
+    </a>`
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', markup);
+
+let stern = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+});
