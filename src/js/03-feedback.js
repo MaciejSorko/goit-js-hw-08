6928619625934b81d90b8a36d117ae883704b49a
ElchaX <maciej.sorko@gmail.com>
2023-03-25 22:15:51 +0100

    3 tasks done

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+function saveMessage(evt) {
+  evt.preventDefault();
+  {
+    const values = JSON.stringify({
+      message: form.elements.message.value,
+      email: form.elements.email.value,
+    });
+    localStorage.setItem('feedback-form-state', values);
+  }
+}
+
+form.addEventListener('input', throttle(saveMessage, 500));
+
+const savedText = JSON.parse(localStorage.getItem('feedback-form-state'));
+form.elements.message.value = savedText.message;
+form.elements.email.value = savedText.email;
+
+function clear(evt) {
+  evt.preventDefault();
+  if (form.elements.message.value !== '' && form.elements.email.value !== '') {
+    console.log(form.elements.email.value);
+    console.log(form.elements.message.value);
+    form.elements.message.value = '';
+    form.elements.email.value = '';
+  } else {
+    alert('wszystkie pola powinny zostać wypełnione');
+  }
+}
+
+form.addEventListener('submit', clear);
